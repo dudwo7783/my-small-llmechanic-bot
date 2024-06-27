939f84ea241e34dbe389985cf315ff1c87ffeaf4
jack lee <aletsire@gmail.com>
2024-06-27 12:41:42 +0900

    Modify app_jk and main file

--- app_jk.py
+++ app_jk.py
@@ -5,40 +5,78 @@ from code_rag.rag import QA
 import sqlite3
 import json
 import ast
+import os
 
 from langchain.agents import initialize_agent
 from langchain.agents.tools import Tool
 from langchain.chains import LLMChain
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_message_histories import RedisChatMessageHistory
 
 import httpx
 import asyncio
 from httpx import TimeoutException
 
-async def get_streaming_response(namespace, query):
+import redis
+
+# TODO: Put Redis Server
+REDIS_URL = "REDIS SERVER URL"
+session_id = "999"
+
+async def get_streaming_response(namespace, query, session_id):
     async with httpx.AsyncClient(timeout=None) as client:
-        async with client.stream('GET', 'http://localhost:8000/aget_car_information/', params={'namespace': namespace, 'query': query}) as response:
-            # TODO: ver 1
-            # TODO: json 분리 확인 1 response['text']
-            # TODO: 안되면 아래처럼
+        async with client.stream('GET', 'http://localhost:8000/aget_car_information/', params={'namespace': namespace, 'query': query, 'session_id': session_id}) as response:
             async for chunk in response.aiter_text():
                 yield chunk
 
-class ChatBot:
-    def __init__(self):
+class ChatBot():
+    def __init__(self, personal_id=999):
         self.messages = []
         self.type=0
         self.id=1
         self.answer = []
         self.set_num = -999
-    # 글자 타이핑 되도록 이펙트
-    def stream_data(self, text):
-        st.toast("답변 생성중입니다......")
-        for word in text.split(" "):
-            yield word + " "
-            time.sleep(0.2)
+    def reset_chat(self, personal_id):
+        st.session_state.chat_bot = ChatBot(personal_id)
+        st.rerun()
 
+    def reset_button(self):
+        self.type = 0
+        self.set_num=-999
+        self.id = 1
+        self.answer = []
+
+    def reset_messages(self, r, redis_messages):
+        self.messages=[]
+        key = [key for key in st.session_state.key_list if b'image' in key and st.session_state.key in key]
+        val = []
+        if len(key)==1:
+            result = list(reversed(r.lrange(key[0], 0, -1)))
+            result = [json.loads(item.decode('utf-8')) for item in result]
+
+            for res in result:
+                image = ast.literal_eval(res['data']['content'])
+                val.append(image)
+
+        else:
+            # key는 하나여야만 함
+            print('FAIL: Too Many Keys')
+
+        for ind, mes in enumerate(redis_messages):
+            content = {}
+            content['text']=mes
+            if ind%2:
+                if len(val)!=0:
+                    content['image'] = val[ind//2]
+                else: 
+                    content['image'] = []
+                self.messages.append({"role": "assistant", "content": content, "answer_type": 'image'})
+            else:
+                content['image'] = []
+                self.messages.append({"role": "user", "content": content, "answer_type": 'image'})
+        return
+    
     # Button 클릭 시 작동
     def display_question_buttons(self, answer_type):
         conn = sqlite3.connect('button.db')
@@ -46,31 +84,32 @@ class ChatBot:
         cursor.execute("SELECT * FROM button WHERE id=?", (self.id,))
         result = cursor.fetchone()
         response = json.loads(result[2])
-        # print(result)
+        
         if result[4] == 0:
             with st.chat_message("assistant"):
                 for index, res in enumerate(response):
-                    print(res, index)
                     ind = str(self.id) + '_' + str(index)
+
                     if st.button(res, key=f"question_{ind}", type="primary"):
+                        st.session_state.clicked = True
                         cursor.execute("SELECT * FROM button WHERE question=?", (res,))
                         self.answer = cursor.fetchone()
                         if self.answer[4]==2:
-                            self.messages.append({"role": "user", "content": res})
+                            user_res = {"text": res, 'image': []}
+                            self.messages.append({"role": "user", "content": user_res})
                             self.type = 1
                         else:
-                            # TODO: self.id정의를 끝에서 하되 여기서 누른것의 id를 self.id로 하는 것을 마지막에 추가해줘야함 그럼 breakㅠ필요 없음
-                            st.session_state.clicked = True
+                            
                             text = response
-                            assis_res = {"text": [text, res], 'image': None}
+                            assis_res = {"text": [text, res], 'image': []}
                             self.messages.append({"role": "assistant", "content": assis_res, "answer_type": answer_type})
-                            self.messages.append({"role": "user", "content": res})
+                            user_res = {"text": res, 'image': []}
+                            self.messages.append({"role": "user", "content": user_res})
                 else:
                     if self.answer:
                         self.set_num = self.answer[0]
             if self.set_num != -999:
                 self.id = self.set_num
-            # st.rerun()
             if st.session_state.clicked:
                 st.session_state.clicked = False
                 st.rerun()
@@ -79,9 +118,8 @@ class ChatBot:
             text = json.loads(result[2])[0]
             image_paths = json.loads(result[3])
             res = {"text": text, "image": image_paths}
-            question = result[1]
+            # question = result[1]
             answer_type='image'
-            # self.messages.append({"role": "user", "content": question})
             self.messages.append({"role": "assistant", "content": res, "answer_type": answer_type})
             with st.chat_message("assistant"):
                 
@@ -92,17 +130,13 @@ class ChatBot:
                         for i, image_path in enumerate(image_paths):
                             with cols[i]:
                                 st.image(image_path)
-            
             asyncio.run(self.handle_user_input(answer_type))
-            # Display a message while waiting for the response
 
     def display_chat_history(self):
         for message in self.messages:
             with st.chat_message(message["role"]):
                 content = message["content"]
                 if message["role"]=='assistant':
-                    # print("displayChat: content")
-                    # print(content)
                     if message['answer_type']=='image':
                         st.markdown(content["text"])
                     else:
@@ -112,27 +146,37 @@ class ChatBot:
                             else:
                                 st.button(res, type="primary")
                 else:
-                    st.markdown(content)
+                    st.markdown(content["text"])
                 
                 if message["role"]=='assistant' and content["image"] != None and message["answer_type"] == "image":
                     image_paths = content["image"]
                     if len(image_paths) != 0:
                         with st.expander("Click to view images"):
-                            cols = st.columns(len(image_paths))
+                            if len(image_paths)<4:
+                                cols = st.columns(len(image_paths))
+                            else:
+                                cols = st.columns(4)
                             for i, image_path in enumerate(image_paths):
-                                # print("#1 Image Path")
-                                # print(image_path)
-                                with cols[i]:
+                                with cols[i%4]:
                                     st.image(image_path)
 
     async def handle_user_input(self, answer_type):
-        user_input = st.chat_input("질문할 내용을 적어주세요: ", key="user_input")
+
+        col1, col2 = st.columns([8.5,1.5])
+        with col1:
+            user_input = st.chat_input("질문할 내용을 적어주세요: ", key="user_input")
+        with col2:
+            if st.button('버튼 질문', key=f"reset_button", type="primary"):
+                answer_type = "button"
+                self.reset_button()
+                st.rerun()
+
         if user_input:
-            
             st.chat_message("user").write(user_input)
             st.toast("서칭 중......")
-            self.messages.append({"role": "user", "content": user_input})
-            st.toast('Yeaaaaaaaaah',  icon='🎉')
+            user_question = {"text": user_input, "image": []}
+            self.messages.append({"role": "user", "content": user_question})
+            # st.toast('Yeaaaaaaaaah',  icon='🎉')
             with st.chat_message("assistant"):
                 
                 with st.spinner("Waiting for response..."):
@@ -144,13 +188,10 @@ class ChatBot:
                         image_paths = ''
                         answer = ''
                         # Get the streaming response from FastAPI
-                        async for chunk in get_streaming_response('SONATA_DN8_2024', user_input):
-                            print(chunk)
+                        async for chunk in get_streaming_response(st.session_state.car_model, user_input, st.session_state.session_id):
                             buffer += chunk.encode('utf-8')  # chunk를 바이트로 변환
                             parts = buffer.split(boundary)
                             
-                            # buffer = parts.pop()
-                            # print(buffer)
                             for part in parts:
                                 part_data = part.split(b"\r\n\r\n")
 
@@ -158,99 +199,110 @@ class ChatBot:
                                 header, data = part_data[0], part_data[1]
 
                                 if b"Content-Type: text/event-stream" in header:
-                                    # print('after if')
-                                    # print(data)
-                                    # logger.info(data.decode("utf-8"))
-                                    # await asyncio.sleep(0.1)
-                                    token = data.decode("utf-8")  # .rstrip()
+                                    token = data.decode("utf-8")
                                     answer += token
-                                    
+                                    container.markdown(answer)
                                 elif b"Content-Type: text/plain" in header:
-                                    # image_data.append(data.strip())
-                                    image_paths = data.decode('utf-8')  # 이미지 경로를 문자열로 변환     
-                            # else:
-                            #     temp = answer
-                            container.markdown(answer)# unsafe_allow_html=True
-                                
-                        else:
-                            # print('hello?')
-                            # print(image_paths)
+                                    image_paths = data.decode('utf-8')  # 이미지 경로를 문자열로 변환  
+                        
+                        try:
                             image_paths = ast.literal_eval(image_paths)
-                            container.markdown(answer)# unsafe_allow_html=True
-                            with container.expander("Click to view images"):
-                                if len(image_paths) != 0:
-                                    cols = st.columns(len(image_paths))
-                                    for i, image_path in enumerate(image_paths):
-                                        with cols[i]:
-                                            st.image(image_path)
                             assistant_response = {"text": answer, "image": image_paths}
-                            self.messages.append({"role": "assistant", "content": assistant_response, "answer_type": answer_type})
+                        except:
+                            assistant_response = {"text": answer, "image": []}
+                        
+                        with container.container():
+                            st.markdown(answer)
+                            if len(image_paths) != 0:
+                                with st.expander("Click to view images"):
+                                    if len(image_paths)<4:
+                                        cols = st.columns(len(image_paths))
+                                    else:
+                                        cols = st.columns(4)
+                                    for i, image_path in enumerate(image_paths):
+                                        
+                                        with cols[i%4]:
+                                            st.image(image_path)
+
+                        self.messages.append({"role": "assistant", "content": assistant_response, "answer_type": answer_type})
                     except TimeoutException:
                         pass
 
+
     def side_bar(self):
         with st.sidebar:
+            
             car_model = st.selectbox(
                 "차정",
-                ("그랜저", "아이오닉 5", "싼타페", "쏘나타")
+                ["IONIQ5_2024", "SANTAFE_MX5_2024", "SONATA_DN8_2024"]
             )
+            r= redis.Redis(host='43.200.165.177', port=6379)
             st.session_state.car_model = car_model
-            for ind, chat in enumerate(['./messages_1.txt', './messages_2.txt', './messages_3.txt'], 1):
-                with open(chat, "r") as file:
-                    # 파일 내용 읽기
-                    content = file.read()
-                    # JSON 형식으로 변환
-                    temp = json.loads(content)
-                first_text = temp[-1]['content']['text'][:10]
-                if st.button(f'Chat_{ind}: {first_text}', key=f"side_bar_{ind}", type="secondary"):
-                    st.session_state.clicked = True
-                    self.messages = temp
+            key_list = r.keys(f'message_store:{st.session_state.personal_id}*')
+            key_list = [key for key in key_list if b'image' not in key]
+            print(key_list)
+            if len(key_list)>0:
+                for index, key in enumerate(key_list, start=1):
+
+                    result = list(reversed(r.lrange(key, 0, -1)))
+                    conv = result[-2]
+                    first_text = json.loads(conv.decode())['data']['content'][:16]
+                    if st.button(f'Chat_{index}: {first_text}', key=f"side_bar_{key}", type="secondary"):
+                        # st.session_state.session_id = 
+                        st.session_state.clicked = True
+                        st.session_state.key = key
+                        redis_messages = list(map(lambda conv: json.loads(conv.decode())['data']['content'], result))
+                        st.session_state.session_id = f'{st.session_state.personal_id}_{index}' 
+                        self.reset_messages(r, redis_messages)
+            else:
+                index = 0
+            st.session_state.max_session = index
             if st.session_state.clicked:
                 st.session_state.clicked = False
-                self.type = 1
+                # self.type = 1
                 st.rerun()
-
+            if st.button("New Chat", key="new_chat"):
+                new_session = st.session_state.max_session + 1
+                st.session_state.session_id = f'{st.session_state.personal_id}_{new_session}' 
+                self.reset_chat(st.session_state.personal_id)
+                st.rerun()
 
     def run(self):
         print("START")
         self.side_bar()
-        # with st.spinner("Loading..."):
-        #     time.sleep(5)
-        # st.success("Done!")
         st.title(f"My Small Javis ver.{st.session_state.car_model}")
-        # 첫 클릭 후에도 first, third in 둘다 발생
-        # TODO: 버튼을 눌렀을 경우 바로 message에 append 되지 않음
-        print(self.messages)
+
         if len(self.messages) == 0:
-            print("First if IN")
             answer_type = "button"
             self.display_question_buttons(answer_type)
 
-        print("First if AFTER")
         self.display_chat_history()
         if len(self.messages) > 0:
-            # print(1)
-            # print("Second if IN")
             last_message = self.messages[-1]
             answer_type = last_message.get("answer_type", "image")
             if self.type==0:
-                # print(2)
-                print("Third if IN")
                 answer_type = "button"
                 self.display_question_buttons(answer_type)
             else:
-                # print(3)
-                print("Forth if IN")
                 answer_type="image"
                 asyncio.run(self.handle_user_input(answer_type))
         print("END")
 
 if "chat_bot" not in st.session_state:
     st.session_state.clicked = False
-    st.session_state.personal_id=-999
-    st.session_state.car_model = '아이오닉 5'
+    st.session_state.personal_id=999
+    st.session_state.car_model = 'IONIQ5_2024'
 
-    st.session_state.chat_bot = ChatBot()
+    r= redis.Redis(host='43.200.165.177', port=6379)
+    key_list = r.keys(f'message_store:{st.session_state.personal_id}*')
+    st.session_state.key_list = key_list
+    key_list = [key for key in key_list if b'image' not in key]
+    if len(key_list)!=0:
+        temp = len(key_list) +1
+        st.session_state.session_id = f'{st.session_state.personal_id}_{temp}'
+    else:
+        st.session_state.session_id = f'{st.session_state.personal_id}_1'
+    st.session_state.chat_bot = ChatBot(st.session_state.personal_id)
 
 st.session_state.chat_bot.run()
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from starlette.middleware.sessions import SessionMiddleware
 from PIL import Image
 import os
 import io
 from io import BytesIO
 import uvicorn
+import asyncio
 
 import base64
 
@@ -13,32 +15,37 @@ from fastapi.responses import StreamingResponse, Response
 from transformers import pipeline
 from rag.car_manual_bot import car_manual_generator
 
-from aiohttp import MultipartWriter, web
+from fastapi.responses import JSONResponse
+from fastapi_sessions.backends.implementations import InMemoryBackend
+from fastapi_sessions.session_verifier import SessionVerifier
+from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
 
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_ENDPOINT"]="https://api.smith.langchain.com"
-# TODO: 바꾸기
-os.environ["LANGCHAIN_API_KEY"]="LANGCHAIN_API_KEY"
+# TODO: LangChain API Key
+os.environ["LANGCHAIN_API_KEY"]="LangChain API Key"
 os.environ["LANGCHAIN_PROJECT"]="my-small_mechanic"
 
-os.environ['OPENAI_API_KEY'] = 'OPENAI_API_KEY'
+# TODO: OpenAI API Key
+os.environ['OPENAI_API_KEY'] = 'OpenAI API Key'
 os.environ['MILVUS_PORT'] = '19530'
 
-milvus_host = 'localhost'#os.environ["MILVUS_HOST"]
+# milvus_host = 'localhost'
+# TODO:EC2 SERVER IP
+milvus_host = 'EC2 SERVER IP'
 milvus_port = os.environ["MILVUS_PORT"]
 
-DB_COLLECTION_NAME = "TEST"
+DB_COLLECTION_NAME = "HYNDAI_CAR_MANUAL"
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 NAMESPACE_TYPE = ["IONIQ5_2024", "SANTAFE_MX5_2024", "SONATA_DN8_2024"]
 # NAMESPACE = NAMESPACE_TYPE[0]
 
-CONTEXT_PATH = 'C:/LLM/streamlit/pdf_context'
-sparse_params = {"drop_ratio_search": 0.01}
-dense_params = {"ef": 100}
+# TODO: Context Path
+CONTEXT_PATH = 'Context Path'
 device='cpu'
 reranker = pipeline("text-classification", model="Dongjin-kr/ko-reranker", device=device)
-# text_generator = car_manual_generator(OPENAI_API_KEY, namespace, milvus_host, milvus_port, DB_COLLECTION_NAME, 10, rrk_weight=(0, 1),
-#                             score_filter=True, threshold=0.3, drop_duplicates=True, context_path=CONTEXT_PATH, reranker=reranker)
+
+
 app = FastAPI()
 
 def from_image_to_bytes(img):
@@ -56,51 +63,40 @@ def from_image_to_bytes(img):
     return decoded
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-# @app.get("/get_car_information/")
-# def generate_car_manual_answer(q: str = None):
+async def read_root(id:int, namespace:str):
+    return
     
-#     reduce_answer, context_answer, context_bag, docs, scores =  text_generator.generate_answer(q)
-    
-#     pil_image_list = []
-#     for img_url in context_bag['image_urls']:
-#         img_abs_path = os.path.join(CONTEXT_PATH + '/' + img_url)
-#         img = Image.open(img_abs_path)
-#         pil_image_list.append(from_image_to_bytes(img))
-#     return {"query": q, "answer": reduce_answer, 'image': pil_image_list}
 
 @app.get("/aget_car_information/")
-async def agenerate_car_manual_answer(namespace: str, query: str):
-    # print(query)
+async def agenerate_car_manual_answer(namespace: str, query: str, session_id: str):
     stream_it = AsyncIteratorCallbackHandler()
     text_generator = car_manual_generator(OPENAI_API_KEY, namespace, milvus_host, milvus_port, DB_COLLECTION_NAME, 10, rrk_weight=(0, 1),
                                 score_filter=True, threshold=0.3, drop_duplicates=True, context_path=CONTEXT_PATH, reranker=reranker)
-    async def generate_response():
-        async for chunk, context_bag in text_generator.agenerate_answer(query, stream_it):
-            yield chunk, context_bag['image_urls']
+
     # 멀티파트 응답 생성
     boundary = "my-custom-boundary"
-    
-    async def iterfile():
-        print('after iterfile')
-        async for chunk, context_bag in generate_response():
+
+    async def generate_response():
+        task = asyncio.create_task(text_generator._agenerate_answer(query, session_id, stream_it))
+        async for chunk in stream_it.aiter():
             print(chunk)
             yield (f"{boundary}\r\n"
                    f"Content-Type: text/event-stream\r\n\r\n"
                    f"{chunk}").encode("utf-8")
-        if len(context_bag)!=0:
-            print(context_bag)
-        #     # TODO: sample
-            image = str(context_bag)
+        if not task.done():
+            await task
+        # get the return value from the wrapped coroutine
+        answer, context_bag = task.result()
+        
+        if (len(context_bag['image_urls'])!=0) or (len(context_bag['table_image_urls'])!=0):
+            image = str(context_bag['image_urls'] + context_bag['table_image_urls'])
         # image = str(['./image/너구리.jpg'])
-        yield (f"{boundary}\r\n"
-                f"Content-Type: text/plain\r\n\r\n"
-                f"{image}").encode("utf-8")
+            yield (f"{boundary}\r\n"
+                    f"Content-Type: text/plain\r\n\r\n"
+                    f"{image}").encode("utf-8")
 
     # 응답 반환
-    return StreamingResponse(iterfile(), media_type=f"multipart/form-data; boundary={boundary}")
+    return StreamingResponse(generate_response(), media_type=f"multipart/form-data; boundary={boundary}")
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
